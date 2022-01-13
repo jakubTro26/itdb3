f0301412ef9699e11cb1a3b2d483ff7b31d7983c
kuba <info@weblead.pl>
2022-01-13 10:14:09 +0100

    pierwszy

--- wp-content/themes/twentynineteen/index.php
+++ wp-content/themes/twentynineteen/index.php
@@ -18,7 +18,7 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main a">
 
 		<?php
 		if ( have_posts() ) {
--- wp-includes/blocks/post-content.php
+++ wp-includes/blocks/post-content.php
@@ -49,7 +49,7 @@ function render_block_core_post_content( $attributes, $content, $block ) {
 		return '';
 	}
 
-	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'entry-contentt' ) );
+	$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'entry-content' ) );
 
 	return (
 		'<div ' . $wrapper_attributes . '>' .
