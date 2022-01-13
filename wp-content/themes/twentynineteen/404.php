a5e09d97a09384b4dbbbf89e0fb2120479e20a71
kuba <info@weblead.pl>
2022-01-13 10:15:22 +0100

    pierwszy

--- wp-content/themes/twentynineteen/404.php
+++ wp-content/themes/twentynineteen/404.php
@@ -13,7 +13,7 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main g">
 
 			<div class="error-404 not-found">
 				<header class="page-header">
